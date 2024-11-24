672c7b6042afb65b465575d0475fc0f095c0eaeb
naiyar2000 <naiyarimam.2000@gmail.com>
2024-11-25 02:10:54 +0530

    mobile-responsive intial effort

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,26 +4,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <div className="relative" style={{ backgroundColor: "#5a848c" }}>
+    <div className="relative h-full" style={{ backgroundColor: "#5a848c" }}>
+      <div className="absolute rounded-sm bg-white text-black bg-opacity-50 top-20 left-5 md:bg-none md:bg-opacity-0 md:top-1/3 md:w-1/3 md:left-16 md:text-white flex flex-col gap-2">
+        <span className="text-lg font-bold md:text-xl md:font-medium">{"WEBSITES > PORTFOLIOS"}</span>
+        <h1 className="font-bold text-6xl pr-4">Create a portfolio website</h1>
+        <div className="pr-24 flex flex-col gap-4 items-start">
+          <h2 className="font-semibold text-xl leading-6">{"Showcase your work online with a portfolio website. Get started with a professionally designed template that can be customized to fit your brand."}</h2>
+          <a href="/pdf/Naiyar_resume.pdf" download="Naiyar_Imam" className="no-underline bg-white text-slate-700 px-10 py-4">{"DOWNLOAD CV"}</a>
+        </div>
+      </div>
+      <div className="h-screen md:h-auto">
         <Image
           src={`${prefix}/images/hero-image.jpg`}
           alt=""
-          style={{
-            width: '100%',
-            height: 'auto',
-          }}
+          className="h-3/4 w-full object-cover object-[center_bottom] md:w-full md:h-auto"
           width={5000}
-          height={3000}
+          height={200}
         />
-        <div className="absolute top-1/3 w-1/3 left-16 flex flex-col gap-2">
-          <span className="text-xl font-medium">{"WEBSITES > PORTFOLIOS"}</span>
-          <h1 className="font-bold text-6xl pr-4">Create a portfolio website</h1>
-          <div className="pr-24 flex flex-col gap-4 items-start">
-            <h2 className="font-semibold text-xl leading-6">{"Showcase your work online with a portfolio website. Get started with a professionally designed template that can be customized to fit your brand."}</h2>
-            <a href="/pdf/Naiyar_resume.pdf" download="Naiyar_Imam" className="no-underline bg-white text-slate-700 px-10 py-4">{"DOWNLOAD CV"}</a>
-          </div>
-        </div>
       </div>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useScrollPosition } from "@/hooks/useScrollPosition";
+import Image from "next/image";
 import Link from "next/link";
 
 const Header = () => {
@@ -8,16 +9,24 @@ const Header = () => {
     const { scrollPosition } = useScrollPosition();
 
     return (
-        <div className={`w-full ${scrollPosition > 0 ? "bg-black" : "bg-transparent"} px-14 py-2 h-20 flex justify-between items-center fixed top-0 z-10`}>
+        <div className={`w-full ${scrollPosition > 0 ? "bg-black" : "bg-transparent"} px-5 md:px-14 py-2 h-20 flex justify-between items-center fixed top-0 z-10`}>
             <div>
                 <Link href={"/"}>
                     <span className="font-bold from-neutral-400 text-2xl">Portfolio</span>
                 </Link>
             </div>
-            <div className="flex gap-4">
+            <div className="hidden md:flex gap-4">
                 <Link href={"/work"}>Work</Link>
                 <Link href={"/template"}>Templates</Link>
                 <Link href={"/resources"}>Resources</Link>
+            </div>
+            <div className="md:hidden">
+                <Image
+                    src={"/icons/hamburgerIcon.svg"}
+                    alt={"option"}
+                    width={40}
+                    height={30}
+                />
             </div>
         </div>
     )

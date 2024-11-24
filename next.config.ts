7f2fece4d1fcfde6e234ad3a08c2bab60bcdc36f
naiyar2000 <naiyarimam.2000@gmail.com>
2024-11-24 21:47:27 +0530

    config changes for production deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
 const nextConfig = {
-  basePath: "/Portfolio",
-  output:  process.env.NODE_ENV !== "production" ? undefined: "export", 
+  basePath: process.env.NODE_ENV !== "production" ? undefined : "/Portfolio",
+  assetPrefix: process.env.NODE_ENV !== "production" ? undefined : "/Portfolio/",
+  output: process.env.NODE_ENV !== "production" ? undefined : "export",
   images: {
     unoptimized: envImageUnoptimize,
     remotePatterns: [

f7068fdea8a0d651528dbda90fc6092a74f40ac3
naiyar2000 <naiyarimam.2000@gmail.com>
2024-11-26 00:15:22 +0530

    a few more section to home page added

--- src/components/Home/PortfolioDescription.tsx
+++ src/components/Home/PortfolioDescription.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import Link from 'next/link';
 import React, { useState } from 'react'
 
-let layoutTypes = [
+const layoutTypes = [
     {
         title: "Grid: Overlay",
         src: "grid-overlay-1000w.png"

08619b9d56493752ba8a41dcf5222c292134455a
naiyar2000 <naiyarimam.2000@gmail.com>
2025-03-05 17:58:10 +0530

    workflow fixes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
 const nextConfig = {
   basePath: process.env.NODE_ENV !== "production" ? undefined : "/Portfolio",
   assetPrefix: process.env.NODE_ENV !== "production" ? undefined : "/Portfolio/",
-  output: process.env.NODE_ENV !== "production" ? undefined : "export",
+  // output: process.env.NODE_ENV !== "production" ? undefined : "export",
+  output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
